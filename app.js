003d4bdebde7ebe9d477a264643a8e70ee87e25c
TIBOlion <158031123+TIBOlion@users.noreply.github.com>
2024-12-17 16:32:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAzaZksGMriLJnbuGcoyHh4-rTdLlQ7BAM",
   authDomain: "einkaufsliste-bc7cf.firebaseapp.com",
@@ -11,27 +8,3 @@ const firebaseConfig = {
   measurementId: "G-4EM9M7DPR7"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-const itemInput = document.getElementById("item-input");
-const addButton = document.getElementById("add-button");
-const itemsList = document.getElementById("items-list");
-
-addButton.addEventListener('click', () => {
-    const newItem = itemInput.value.trim();
-    if (newItem) {
-        push(ref(db, 'items'), newItem);
-        itemInput.value = '';
-    }
-});
-
-onValue(ref(db, 'items'), (snapshot) => {
-    itemsList.innerHTML = '';
-    snapshot.forEach(childSnapshot => {
-        const item = childSnapshot.val();
-        const li = document.createElement('li');
-        li.textContent = item;
-        itemsList.appendChild(li);
-    });
-});
