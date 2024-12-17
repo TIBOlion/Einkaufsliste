823f03a4f11b962759f99dafd46472274c4d5dc6
TIBOlion <158031123+TIBOlion@users.noreply.github.com>
2024-12-17 14:51:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
   apiKey: "AIzaSyAzaZksGMriLJnbuGcoyHh4-rTdLlQ7BAM",
   authDomain: "einkaufsliste-bc7cf.firebaseapp.com",
@@ -17,16 +8,13 @@ const firebaseConfig = {
   measurementId: "G-4EM9M7DPR7"
 };
 
-// Firebase initialisieren
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-// DOM-Elemente
 const itemInput = document.getElementById("item-input");
 const addButton = document.getElementById("add-button");
 const itemsList = document.getElementById("items-list");
 
-// Funktion zum Hinzufügen eines neuen Produkts
 addButton.addEventListener('click', () => {
     const newItem = itemInput.value.trim();
     if (newItem) {
@@ -35,7 +23,6 @@ addButton.addEventListener('click', () => {
     }
 });
 
-// Funktion zum Auflisten der Produkte und Zuhören auf Änderungen
 db.ref('items').on('value', (snapshot) => {
     itemsList.innerHTML = '';
     snapshot.forEach(childSnapshot => {
@@ -45,7 +32,3 @@ db.ref('items').on('value', (snapshot) => {
         itemsList.appendChild(li);
     });
 });
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
