ae8f7a6015870f62292b5dc73c2e070fe3533222
TIBOlion <158031123+TIBOlion@users.noreply.github.com>
2024-12-17 17:15:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
  export const firebaseConfig = {
   apiKey: "AIzaSyAzaZksGMriLJnbuGcoyHh4-rTdLlQ7BAM",
   authDomain: "einkaufsliste-bc7cf.firebaseapp.com",
+  databaseURL: "https://einkaufsliste-bc7cf-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "einkaufsliste-bc7cf",
   storageBucket: "einkaufsliste-bc7cf.firebasestorage.app",
   messagingSenderId: "270069108055",
